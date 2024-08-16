61953ea93a1dd7cf971d13c7293ed3bdd71b8a88
leeeydia <leeeydia@gmail.com>
2024-08-16 16:30:53 +0900

    2차 과제

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from "react"; 
 import TodoList from './components/TodoList'
 import './App.css'
+  
 
 
 function App() {
   return(
-    <main className="main-center">
-
+<main className="main-center">
 <TodoList/>
     </main>
   )
 }
 export default App
+
+
+// const [todos, setTodos] = useState([]);
+// [상태 변화값, 변화시키기 위한 함수]
+// 공부
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 const TodoList = () => {
-  const todos = [
-    { id: 1, text: "Buy milk" },
+  const sampleTodos = [
+  { id: 1, text: "Buy milk" },
   { id: 2, text: "Clean the house" },
   { id: 3, text: "Go for a run" },
   { id: 4, text: "Finish homework" },
@@ -14,12 +14,44 @@ const TodoList = () => {
   { id: 9, text: "Do laundry" },
   { id: 10, text: "Write code" },
   ]
+  const [todos, setTodos] = useState(sampleTodos)
+  const [todo, setTodo] = useState([])
+  
+  const handleChange=(event) => {
+    console.log(event.target.value)
+    setTodo(event.target.value)
+  
+  }
+  const handleSubmit = () => {
+    event.preventDefault();
+  
+    if(todo.trim().length ==0){
+      alert('값을 입력해 주세요')
+    return;  
+  }  
+    let newTodos = {
+      id: crypto.randomUUID(),
+      text : todo
+    }
+    setTodos([...todos,newTodos]);
+    setTodo("")
+  }
+
   return (
+  
+    <div>
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="todo를 입력해주세요"
+      onChange={handleChange}
+      value={todo}/>
+      <button>추가하기</button>
+    </form>
     <ul>
     {todos.map((todo) => (
       <li key={todo.id}>{todo.text}</li>
     ))}
   </ul>
+  </div>
   )
 }
 export default TodoList
