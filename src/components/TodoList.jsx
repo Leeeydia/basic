af2c416367b8bb4d00ac53b882ac829ab2d762e7
leeeydia <leeeydia@gmail.com>
2024-08-20 14:41:03 +0900

    3회차 과제

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,57 +1,81 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
+const sampleTodos = [
+  { id: 1, text: "Buy milk", completed: false },
+  { id: 2, text: "Clean the house", completed: false },
+  { id: 3, text: "Go for a run", completed: false },
+  { id: 4, text: "Finish homework", completed: false },
+  { id: 5, text: "Call mom", completed: false },
+  { id: 6, text: "Buy groceries", completed: false },
+  { id: 7, text: "Walk the dog", completed: false },
+  { id: 8, text: "Read a book", completed: false },
+  { id: 9, text: "Do laundry", completed: false },
+  { id: 10, text: "Write code", completed: false },
+];
 
 const TodoList = () => {
-  const sampleTodos = [
-  { id: 1, text: "Buy milk" },
-  { id: 2, text: "Clean the house" },
-  { id: 3, text: "Go for a run" },
-  { id: 4, text: "Finish homework" },
-  { id: 5, text: "Call mom" },
-  { id: 6, text: "Buy groceries" },
-  { id: 7, text: "Walk the dog" },
-  { id: 8, text: "Read a book" },
-  { id: 9, text: "Do laundry" },
-  { id: 10, text: "Write code" },
-  ]
-  const [todos, setTodos] = useState(sampleTodos)
-  const [todo, setTodo] = useState([])
-  
-  const handleChange=(event) => {
-    console.log(event.target.value)
-    setTodo(event.target.value)
-  
-  }
-  const handleSubmit = () => {
-    event.preventDefault();
-  
-    if(todo.trim().length ==0){
-      alert('값을 입력해 주세요')
-    return;  
-  }  
-    let newTodos = {
-      id: crypto.randomUUID(),
-      text : todo
-    }
-    setTodos([...todos,newTodos]);
-    setTodo("")
-  }
+  const [todos, setTodos] = useState(sampleTodos);
 
+  const [todo, setTodo] = useState([]);
+
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    setTodo(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!todo.trim()) {
+      return;
+    }
+
+    setTodos([{ id: crypto.randomUUID(), text: todo }, ...todos]);
+    setTodo("");
+  };
+  const toggleCompleted = (id) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        const newTodo = {
+          ...todo,
+          completed: !todo.completed,
+        };
+
+        return newTodo;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
   return (
-  
     <div>
-    <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="todo를 입력해주세요"
-      onChange={handleChange}
-      value={todo}/>
-      <button>추가하기</button>
-    </form>
-    <ul>
-    {todos.map((todo) => (
-      <li key={todo.id}>{todo.text}</li>
-    ))}
-  </ul>
-  </div>
-  )
-}
-export default TodoList
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="todo를 입력해주세요"
+          onChange={handleChange}
+          value={todo}
+        />
+        <button>추가하기</button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <p>
+              {todo.text} - {String(todo.completed)}
+            </p>
+            <button
+              onClick={() =>
+                toggleCompleted(todo.id, todo.text, todo.completed)
+              }
+            >
+              완료
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default TodoList;
+
+// 업데이트 togg
